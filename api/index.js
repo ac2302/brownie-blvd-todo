87340492ebacc35d3aa33028e4294d4edd0c55c3
Aryan Chavan <aryan@aryansdomain.com>
2021-07-10 15:52:19 +0530

    add GET /orders

--- api/index.js
+++ api/index.js
@@ -13,9 +13,55 @@ mongoose.connect(
 	}
 );
 
-// test route
-app.get("/", (req, res) => {
-	res.send("hello world");
+// schema
+const ChocolateType = mongoose.model(
+	"ChocolateType",
+	new mongoose.Schema({
+		name: {
+			type: String,
+			required: true,
+		},
+	})
+);
+const Payment = mongoose.model(
+	"Payment",
+	new mongoose.Schema({
+		method: { type: String },
+		paid: { type: Boolean },
+		amount: { type: Number },
+	})
+);
+const Order = mongoose.model(
+	"Order",
+	new mongoose.Schema({
+		cocolateType: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "ChocolateType",
+		},
+		dueDate: {
+			type: Date,
+		},
+		hasToBeDelivered: {
+			type: Boolean,
+		},
+		nameOnCard: {
+			type: String,
+		},
+		payment: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Payment",
+		},
+	})
+);
+
+// get all orders
+app.get("/orders", (req, res) => {
+	Order.find({}, (err, docs) => {
+		if (err) res.statusCode(500).json({ err });
+		else {
+			res.json({ orders: docs });
+		}
+	});
 });
 
 const PORT = process.env.PORT;
