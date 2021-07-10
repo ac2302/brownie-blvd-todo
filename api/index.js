24bd9feffef3673ef81eed2c70f7cba822cea561
Aryan Chavan <aryan@aryansdomain.com>
2021-07-10 23:39:06 +0530

    add cors

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 // creating app
 const app = express();
 
 // all route middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require("./middlewares/auth")); // for auth
