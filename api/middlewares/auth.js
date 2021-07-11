ba6dc3d45eb4931d31e864120a4cfb92b635c147
Aryan Chavan <aryan@aryansdomain.com>
2021-07-11 12:09:22 +0530

    update middleware to use jwt authentication

--- api/middlewares/auth.js
+++ api/middlewares/auth.js
@@ -1,30 +1,21 @@
+const jwt = require("jsonwebtoken");
+
 const User = require("../models/User");
 
 module.exports = (req, res, next) => {
-	if (!req.body.user) {
-		// there is no user in body
+	const token = req.headers["auth-token"];
+
+	if (!token) {
 		req.auth = { isAuthenticated: false };
 		next();
-	} else if (
-		!(
-			req.body.user.hasOwnProperty("username") &&
-			req.body.user.hasOwnProperty("password")
-		)
-	) {
-		// the user in body does not have all the required fields
+	}
+
+	try {
+		const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+		req.auth = { isAuthenticated: true, user: verified._id };
+		next();
+	} catch {
 		req.auth = { isAuthenticated: false };
 		next();
-	} else {
-		// valid input
-		// authenticating
-		const { username, password } = req.body.user;
-
-		User.findOne({ username, password }, (err, doc) => {
-			if (err) res.statusCode(500).json({ err });
-			else if (doc === null) req.auth = { isAuthenticated: false };
-			else req.auth = { isAuthenticated: true, user: username };
-
-			next();
-		});
 	}
 };
--- api/middlewares/authOnly.js
+++ api/middlewares/authOnly.js
@@ -1,10 +1,9 @@
 const authOnly = (req, res, next) => {
-	// if (req.auth.isAuthenticated) next();
-	// else {
-	// 	res.statusCode = 403;
-	// 	res.json({ message: "you need to be logged in to make this request" });
-	// }
-	next();
+	if (req.auth.isAuthenticated) next();
+	else {
+		res.statusCode = 401;
+		res.json({ message: "you need to be logged in to make this request" });
+	}
 };
 
 module.exports = authOnly;
