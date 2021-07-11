f87c08dee2e8b5ad4b983bd4557d841655ddd706
Aryan Chavan <aryan@aryansdomain.com>
2021-07-11 12:52:02 +0530

    add password hashing and salting

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const User = require("../models/User");
 
@@ -14,7 +15,7 @@ router.post("/", async (req, res) => {
 					res.statusCode = 400;
 					res.json({ message: "incorrect username or password" });
 				} else {
-					if (password === currentUser.password) {
+					if (await bcrypt.compare(password, currentUser.password)) {
 						if (!currentUser.isActivated) {
 							res.statusCode = 400;
 							res.json({
--- api/routes/register.js
+++ api/routes/register.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+
 const User = require("../models/User");
 
 router.post("/", async (req, res) => {
@@ -10,7 +12,13 @@ router.post("/", async (req, res) => {
 				res.statusCode = 400;
 				res.json({ message: "username is taken" });
 			} else {
-				const newUser = new User({ username, password, isActivated: false });
+				const salt = await bcrypt.genSalt(10);
+				const hashedPass = await bcrypt.hash(password, salt);
+				const newUser = new User({
+					username,
+					password: hashedPass,
+					isActivated: false,
+				});
 
 				newUser.save((err) => {
 					if (err) {
