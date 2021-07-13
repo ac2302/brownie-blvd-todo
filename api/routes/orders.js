b5acf395af53263dd39338a48f616cedcaaf8869
Aryan Chavan <aryan@aryansdomain.com>
2021-07-13 09:22:52 +0530

    add order sorting

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Order = require("../models/Order");
 
 router.get("/", require("../middlewares/authOnly"), (req, res) => {
-	Order.find({}, (err, docs) => {
+	Order.find({}, null, {sort: {dueDate: 1}}, (err, docs) => {
 		if (err) res.statusCode(500).json({ err });
 		else {
 			res.json({ orders: docs });
